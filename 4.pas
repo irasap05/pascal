6bf5691fe6087dd3622eb926c1975c61af85142b
irasap05 <cvetikksa@gmail.com>
2019-09-22 16:42:05 +0300

    Create 4.pas

---
+++ 4.pas
@@ -0,0 +1,15 @@
+﻿var
+  x, n, s: integer;
+  
+Begin
+  write('введти количество чисел: ');
+  read(n);
+  s := 0;
+  writeln('теперь введите сами числа: ');
+  for var i := 1 to n do
+  begin
+    read(x);
+    s += x;
+  end;
+  write('arithmetic mean = ', s / n);
+end.
