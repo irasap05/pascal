4331e133640cb4bcea1451a03ab5356082556f96
irasap05 <cvetikksa@gmail.com>
2019-09-29 15:42:09 +0300

    Create 3.pas

---
+++ 290919/3.pas
@@ -0,0 +1,8 @@
+﻿var
+  x, y, z : integer;
+  
+Begin
+  read(x, y, z);
+  writelnFormat('{0} + {1} + {2} = {3}, {0} * {1} * {2} = {4}, ({0} + {1} + {2}) / 3 = {5:f6}', 
+      x, y, z, x + y + z, x * y * z, (x + y + z) / 3)
+end.
