944f19f3886f10ee947cee8753710b479fd797ac
irasap05 <cvetikksa@gmail.com>
2019-09-29 16:40:00 +0300

    Create piramid.pas

---
+++ piramid.pas
@@ -0,0 +1,17 @@
+﻿var
+  amountOfSpace, amountOfSymbols, height, width, maxLength: integer;
+  symbol : char;
+  
+begin
+  read(height);
+  symbol := 'o';
+  maxLength := 2 * height - 1;
+  width := (maxLength - 1) div 2;
+  amountOfSymbols := 1;
+  for var level := 1 to height do
+  begin
+    writeln(width * ' ', amountOfSymbols * symbol);
+    width -= 1;
+    amountOfSymbols += 2;
+  end;
+end.
