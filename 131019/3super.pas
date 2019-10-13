d67dae58ac425f02a247ba8901f10b170309d63f
irasap05 <cvetikksa@gmail.com>
2019-10-13 15:53:39 +0300

    Create 3super.pas

---
+++ 131019/3super.pas
@@ -0,0 +1,23 @@
+﻿function min(a, b, c: integer): integer;
+begin
+  result := integer.MaxValue;
+  if a < result then result := a;
+  if b < result then result := b;
+  if c < result then result := c;
+end;
+
+function max(a, b, c: integer): integer;
+begin
+  result := integer.MinValue;
+  if a > result then result := a;
+  if b > result then result := b;
+  if c > result then result := c;
+end;
+
+var
+  a, b, c: integer;
+  
+begin
+  read(a, b, c);
+  write(min(a, b, c) + max(a, b, c));
+end.
