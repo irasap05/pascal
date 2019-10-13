027394e3f5fd52dcec42f09971b9584fc4fd781f
irasap05 <cvetikksa@gmail.com>
2019-10-13 15:37:05 +0300

    2 done

---
+++ 131019/2.pas
@@ -0,0 +1,11 @@
+﻿var
+  a, e, d, c, s: integer;
+  
+Begin
+  read(a);
+  e := a mod 10;
+  c := a div 100;
+  d := (a mod 100) div 10;
+  s := e + c + d;
+  if s mod 2 = 0 then write('сумма четная') else write('сумма нечетная');
+end.
---
+++ 131019/2super.pas
@@ -0,0 +1,17 @@
+﻿function sumOfDigit(n: integer): integer;
+begin
+  result := 0;
+  while(n <> 0) do
+  begin
+    result += n mod 10;
+    n := n div 10;
+  end;
+end;
+
+var
+  a: integer;
+
+begin
+  read(a);
+  if sumOfDigit(a) mod 2 = 0 then write('odd') else write('even');
+end.
