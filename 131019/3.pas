d4340a6b034dd0f37da0a53e6dabe437d1ca59cc
irasap05 <cvetikksa@gmail.com>
2019-10-13 15:48:53 +0300

    Create 3.pas

---
+++ 131019/3.pas
@@ -0,0 +1,19 @@
+﻿var
+  a, b, c, min, max: integer;
+  
+Begin
+  read(a, b, c);
+  
+  min := integer.MaxValue;
+  max := integer.MinValue;
+  
+  if a > max then max := a;
+  if b > max then max := b;
+  if c > max then max := c;
+  
+  if a < min then min := a;
+  if b < min then min := b;
+  if c < min then min := c;
+  
+  write(min + max);
+end.
