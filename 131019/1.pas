47880dd94156c562c5f8615617f81fb6c1b4dc9a
irasap05 <cvetikksa@gmail.com>
2019-10-13 15:17:24 +0300

    1 done

---
+++ 131019/1.pas
@@ -0,0 +1,12 @@
+﻿var
+  a, b, c, d, m: integer;
+  
+Begin
+  read(a, b, c, d);
+  m := integer.MinValue;
+  if a > m then m := a;
+  if b > m then m := b;
+  if c > m then m := c;
+  if d > m then m := d;
+  write(m);
+end.
