ef8137f7db53090b747dd56c344c1c1148253968
Qvocha <pineappletoe@gmail.com>
2019-10-20 15:44:06 +0300

    Create oblast.pas

---
+++ 201019/oblast.pas
@@ -0,0 +1,10 @@
+﻿var
+  x1, x2, x3, x4, y1, y2, y3, y4, x, y: integer;
+  InRectangle1, InRectangle2: boolean;
+
+begin
+  read(x, y);
+  InRectangle1 := (x >= x1)  and (x <= x2) and (y >= y1) and (y <= y2);
+  InRectangle2 := (x >= x3)  and (x <= x4) and (y >= y3) and (y <= y4);
+  if InRectangle1 and not(InRectangle2) then write('Yes') else write('No');
+end.
