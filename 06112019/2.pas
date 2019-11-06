909bfefcf90a4f58a34a4710b6bc46a66fc5780b
Qvocha <pineappletoe@gmail.com>
2019-11-06 16:01:53 +0300

    3

---
+++ 06112019/2.pas
@@ -0,0 +1,9 @@
+﻿var
+  a, b, c, k: integer;
+
+begin
+  read(a, b, c, k);
+  if a mod k = 0 then writelnFormat('{0} является делителем {1}', k, a);
+  if b mod k = 0 then writelnFormat('{0} является делителем {1}', k, b);
+  if c mod k = 0 then writelnFormat('{0} является делителем {1}', k, c);
+end.
---
+++ 06112019/3.pas
@@ -0,0 +1,33 @@
+﻿function max3(a, b, c: integer): integer;
+begin
+  result := integer.MinValue;
+  if a > result then result := a;
+  if b > result then result := b;
+  if c > result then result := c;
+end;
+
+var
+  a, b, c, d, r: integer;
+  flag: boolean;
+
+begin
+  read(a, b, c, d);
+  flag := true;
+  if a = d then 
+  begin  
+    writeln('a = d');
+    flag := false;
+  end;
+  if b = d then
+  begin  
+    writeln('b = d');
+    flag := false;
+  end;
+  if c = d then
+  begin  
+    writeln('c = d');
+    flag := false;
+  end;
+  
+  if flag then write(max3(d - a, d - b, d - c));
+end.
