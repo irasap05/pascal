7baf9bd31ea0990b4660041026994d6de6549bdb
Qvocha <pineappletoe@gmail.com>
2019-11-06 15:45:05 +0300

    Create 1.pas

---
+++ 06112019/1.pas
@@ -0,0 +1,10 @@
+﻿var
+  d, age: integer;
+  
+begin
+  read(age);
+  d := age mod 10;
+  if (d = 1) then write(age, ' год');
+  if (d = 0) or ((d >= 5) and (d <= 9)) then write(age, ' лет');
+  if (d >= 2) and (d <= 4) then write(age, ' года');
+end.
