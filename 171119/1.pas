7785b6c281c7ab4591194b0dfc29a3ce28bf0741
Qvocha <pineappletoe@gmail.com>
2019-11-17 15:59:28 +0300

    Create 1.pas

---
+++ 171119/1.pas
@@ -0,0 +1,25 @@
+﻿var
+  x, a, b, c, m, S, p: integer;
+  
+begin
+  x := random(100, 999);
+  c := x mod 10;
+  b := x div 10 mod 10; 
+  a := x div 100;
+  writeln(a);
+  writeln(b);
+  writeln(c);
+  if a = c then writeln('да') else writeln('нет');
+  if (a < b) and (b < c) then writeln('да') else writeln('нет');
+  m := 0;
+  if a > m then m := a;
+  if b > m then m := b;
+  if c > m then m := c;
+  writeln(m);
+  S := a + b + c;
+  writeln(S);
+  if S mod 6 = 0 then writeln('да') else writeln('нет');
+  if (10 < S) and (S < 15) then writeln('да') else writeln('нет');
+  p := c * 100 + b * 10 + a;
+  writeln(p);
+end.
