603854f01b2795c6c4a720c98ac86ccaf5a5a84e
Qvocha <pineappletoe@gmail.com>
2020-02-24 16:14:57 +0300

    .

---
+++ 240220/2.pas
@@ -0,0 +1,29 @@
+﻿const n = 4;
+begin
+  var m: array[1..n, 1..n] of integer := ((11, 12, 13, 14), (21, 22, 23, 24), (31, 32, 33, 34), (41, 42, 43, 44));
+  for var i := 1 to n do
+  begin
+    for var j := 1 to n do
+      Write(m[i, j], ' ');
+    Writeln();
+  end;
+  for var i := 1 to n do
+  begin
+    var Max := integer.MinValue;
+    var a := -1;
+    for var j := 1 to n do
+      if m[i, j] > max then 
+      begin
+        Max := m[i, j];
+        a := j;
+      end;
+    Swap(M[i, a], m[i, i]);
+  end;
+  Writeln();
+  for var i := 1 to n do
+  begin
+    for var j := 1 to n do
+      Write(m[i, j], ' ');
+    Writeln();
+  end;
+end.
