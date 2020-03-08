2df1bf45044fbb04556fd755e8804cdb065d8bc7
Qvocha <pineappletoe@gmail.com>
2020-03-08 15:42:17 +0300

    ..

---
+++ 080320/1.pas
@@ -0,0 +1,16 @@
+﻿const n = 5;
+begin
+  var a: array[1..n] of integer;
+  var m: array[1..n] of integer;
+  var k: integer;
+  var p := 0;
+  Read(k);
+  for var i := 1 to n do
+    if m[i] mod 10 = k then
+    begin
+      p += 1;
+      a[p] := m[i];
+    end;
+  for var i := 1 to p do
+    Write(a[i], ' ');
+end.
