74b3b21c89f54c35eab8993fc402af0f12183d05
Qvocha <pineappletoe@gmail.com>
2020-03-08 16:51:51 +0300

    .

---
+++ проверочная/1.pas
@@ -0,0 +1,22 @@
+﻿const n = 15;
+begin
+  var o := 0;
+  var max := integer.MinValue;
+  var m: array[1..n] of integer;
+  for var i := 1 to n do
+    m[i] := Random(-10, 10);
+  Writeln(m);
+  var s := 0;
+  for var i := 1 to n do
+  begin
+    if m[i] mod 10 = 3 then s += m[i];
+    if (m[i] > Max) and (i mod 2 = 1) then
+    begin  
+      max := m[i];
+      o := i;
+    end;
+  end;
+  Swap(m[1], m[o]);
+  Writeln(m);
+  Write(s);
+end.
---
+++ проверочная/3.pas
@@ -0,0 +1,27 @@
+﻿const n = 5;
+begin
+var m: array[1..n, 1..n] of integer;
+for var i := 2 to n - 1 do
+begin
+  for var j := 2 to n - 1 do
+    m[i, j] := 1; 
+  m[i, 1] := 0;
+  m[i, n] := 0;
+end;
+   m[1, n] := 1;
+   m[1, 1] := 1;
+   m[n, 1] := 1;
+   m[n, n] := 1;
+   for var i := 2 to n - 1 do
+   begin
+     m[1, i] := 0;
+     m[n, i] := 0;
+   end;
+       
+    for var i := 1 to n do
+    begin
+      for var j := 1 to n do
+        Write(m[i, j], ' ');
+      Writeln();
+    end;
+end.
