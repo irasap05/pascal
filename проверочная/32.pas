b2bacdae6bb27178f1933fc6e440b8855888cb53
Qvocha <pineappletoe@gmail.com>
2020-03-15 18:01:02 +0300

    ааа

---
+++ проверочная/32.pas
@@ -0,0 +1,17 @@
+﻿const n = 10;
+begin
+var m: array[1..n, 1..n] of integer;
+for var i := 1 to n do
+begin
+  for var j := 1 to i - 1 do
+    m[i, j] := 0;
+  for var j := i to n do
+    m[i, j] := j - (i - 1); 
+end;
+for var i := 1 to n do
+begin
+  for var j := 1 to n do
+    write(m[i, j], ' ');
+  writeln();
+end;
+end.
---
+++ проверочная/4.pas
@@ -0,0 +1,35 @@
+﻿const n = 5; m = 6;
+begin
+var m: array[1..m, 1..n] of integer;
+for var i := 1 to m do
+begin
+  for var j := 1 to n do
+  begin
+    m[i, j] := random(1, 10);
+    write(m[i, j], ' ');
+  end;
+  writeln();
+end;
+var min := integer.MaxValue;
+var max := integer.MinValue;
+var o := -1;
+var k := -1;
+for var j := 1 to n do
+begin
+  var s := 0;
+  for var i := 1 to m do
+    s += m[i, j];
+  if s > max then 
+  begin
+    max := s;
+    o := j;
+  end;
+  if s < min then
+  begin
+    min := s;
+    k := j;
+  end;
+end;
+writeln('max = ', o);
+write('min = ', k);
+end.
