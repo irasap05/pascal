48c3b416ed1432744e557882ce27eb9ecd2df74b
Qvocha <43353220+Qvocha@users.noreply.github.com>
2020-10-26 17:06:15 +0300

    .

---
+++ егэ/2.pas
@@ -0,0 +1,38 @@
+﻿procedure f();
+begin
+  
+end;
+
+function p(n: integer) : boolean;
+begin
+  if n = 1 then
+  begin
+    Result := false;
+    exit;
+  end;
+  
+  if n = 2 then
+  begin
+    result := true;
+    exit;
+  end;
+  
+  for var i := 2 to n div 2 + 1 do
+    if n mod i = 0 then
+    begin
+      Result := false;
+      exit;
+    end;
+  
+  Result := true;
+end;
+
+begin
+  var k: integer;
+  k := 0;
+  for var x := 245690 to 245756 do
+    begin
+     k += 1;
+     if p(x) then Writeln(k, ' ', x);
+    end;
+end.
---
+++ егэ/3.pas
@@ -0,0 +1,25 @@
+﻿type pascalGovno = array[1..4] of integer;
+
+function k(var dividers: pascalGovno; n: integer) : integer;
+begin
+  result := 0;
+  for var i := 2 to n div 2 + 1 do
+  if n mod i = 0 then
+  begin
+    result += 1;
+    if result > 4 then exit;
+    dividers[result] := i;
+  end;
+end;
+
+begin
+  for var x := 1 to 100 do
+  begin
+    var d: pascalGovno;
+    var amountOfDividers := k(d, x);
+    if amountOfDividers = 4 then
+    begin
+      Writeln(x, ' ', d);
+    end;
+  end;
+end.
